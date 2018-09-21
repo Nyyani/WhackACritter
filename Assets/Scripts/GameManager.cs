ce09fe9eeda5ab0a4cdc4bb4b42a6f4a3c2d9370
Nyyani <43067342+Nyyani@users.noreply.github.com>
2018-09-21 15:50:00 +0100

    added timer

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public List<GameObject> critterPrefabs;
+    public float critterSpawnFrequency = 1.0f;
+    public Score scoreDisplay;
+
+    private float lastCritterSpawn = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float nextSpawnTime = lastCritterSpawn + critterSpawnFrequency;
+        if (Time.time >= lastCritterSpawn + critterSpawnFrequency)
+        {
+            //It is time!!
+            //Choose a random critter to spawn
+            int critterIndex = Random.Range(0, critterPrefabs.Count);
+            GameObject prefabToSpawn = critterPrefabs[critterIndex];
+
+            //Spawn the critter!
+            GameObject spawnedCritter = Instantiate(prefabToSpawn);
+
+            //Get access to out Critter Script
+            Critter critterScript = spawnedCritter.GetComponent<Critter>();
+
+            //Tell the critter script the score object
+            critterScript.scoreDisplay = scoreDisplay;
+
+            //Update the most recent critter spawn time to now
+            lastCritterSpawn = Time.time;
+        }
+	}
+}
